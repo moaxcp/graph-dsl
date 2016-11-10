e541c41d522c10c182fbaaa3facd9cfdf4574db2
John J Mercier <moaxcp@gmail.com>
2016-11-09 21:56:12 -0500

    fixed Graph

--- src/main/groovy/graph/Graph.groovy
+++ src/main/groovy/graph/Graph.groovy
@@ -2,14 +2,14 @@ package graph
 
 class Graph {
     def vertices = [:]
-    def edges = []
+    def edges = [] as Set
 
     def vertice(name) {
-        vertices[name] = new Vertice(name: name)
+        vertices[name] = new Vertex(name: name)
     }
 
     def vertice(name, closure) {
-        def vertice = new Vertice(name: name)
+        def vertice = new Vertex(name: name)
         closure.delegate = vertice
         closure.call()
         vertices[name] = vertice
@@ -17,27 +17,19 @@ class Graph {
 
     def edge(one, two) {
         def edge = new Edge(one: one, two: two)
-        validateEdge(edge)
-        edges += edge
-    }
-
-    def edge(one, two, closure) {
-        def edge = new Edge(one: one, two: two)
-        validateEdge(edge)
-        closure.delegate = edge
-        closure.call()
-        edges += edge
-    }
-
-    def validateEdge(edge) {
-        if(!validEdge(edge)) {
+        def added = edges.add edge
+        if(!added) {
             throw new IllegalArgumentException("Edge already exists between $edge.one and $edge.two")
         }
     }
 
-    def validEdge(edge) {
-        edges.any {
-            (it.one == edge.one || it.one == edge.two) && (it.two == edge.one || it.two == edge.two)
+    def edge(one, two, closure) {
+        def edge = new Edge(one: one, two: two)
+        closure.delegate = edge
+        closure.call()
+        def added = edges.add edge
+        if(!added) {
+            throw new IllegalArgumentException("Edge already exists between $edge.one and $edge.two")
         }
     }
 }
