118fea4047af227bd0bc2b22ea1822f62aa3e31e
John J Mercier <moaxcp@gmail.com>
2016-11-09 21:55:31 -0500

    adding equals method

--- src/main/groovy/graph/Edge.groovy
+++ src/main/groovy/graph/Edge.groovy
@@ -3,4 +3,21 @@ package graph
 class Edge {
     def one
     def two
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Edge)) {
+            return false
+        }
+        if (this.is(o)) {
+            return true
+        }
+        Edge rhs = (Edge) o;
+        return (one == rhs.one || one == rhs.two) && (two == rhs.one || two == rhs.two)
+    }
+
+    @Override
+    public int hashCode() {
+        one.hashCode() + two.hashCode()
+    }
 }
