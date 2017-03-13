38acfa638df661a189b1341ac63fcae355b0bf6d
John J Mercier <moaxcp@gmail.com>
2017-03-12 20:53:12 -0400

    Fixing codenarc issues.

--- src/main/groovy/graph/DefaultVertexFactory.groovy
+++ src/main/groovy/graph/DefaultVertexFactory.groovy
@@ -1,7 +1,8 @@
 package graph
 
 /**
- * Factory for creating vertices.
+ * Factory for creating vertices. This factory returns instances of the
+ * base class Vertex.
  */
 class DefaultVertexFactory implements VertexFactory {
     @Override
--- src/main/groovy/graph/DirectedEdgeFactory.groovy
+++ src/main/groovy/graph/DirectedEdgeFactory.groovy
@@ -1,8 +1,19 @@
 package graph
 
+/**
+ * Factory for creating edges. This class returns instances of
+ * Directed Edge.
+ */
 class DirectedEdgeFactory implements EdgeFactory {
+
+    /**
+     * Returns a new DirectedEdge with the given parameters.
+     * @param one
+     * @param two
+     * @return a new DirectedEdge
+     */
     @Override
     Edge newEdge(String one, String two) {
-        return new DirectedEdge(one: one, two: two)
+        new DirectedEdge(one:one, two:two)
     }
 }
