d159872f74c3cc1a6b92b65581ac37a313441bda
John J Mercier <moaxcp@gmail.com>
2017-03-27 22:42:20 -0400

    adding javadoc.

--- src/main/groovy/graph/VertexSpec.groovy
+++ src/main/groovy/graph/VertexSpec.groovy
@@ -1,35 +1,65 @@
 package graph
 
+import groovy.transform.PackageScope
+
+/**
+ * Specification class that helps vertex methods in {@link Graph} objects. It can be the delegate of closures used in
+ * vertex methods of {@link Graph}.
+ */
+@PackageScope
 class VertexSpec {
+    /**
+     * Used to set the name of the {@link Vertex}.
+     */
     String name
     private Set<Class> traits = [] as Set<Class>
     private Set<String> connectsTo = [] as Set<String>
     private Closure config
 
+    /**
+     * @return the set of traits that should be applied to the {@link Vertex}
+     */
     Set<Class> getTraits() {
         traits
     }
 
+    /**
+     * {@link Graph} will use this set to create edges between the {@link Vertex} and other vertices.
+     * @return The names of vertices the {@link Vertex} should connect to.
+     */
     Set<String> getConnectsTo() {
         connectsTo
     }
 
-    String getName() {
-        name
-    }
-
+    /**
+     * @return The config applied to the {@link Vertex} using its leftShift operator.
+     */
     Closure getConfig() {
         config
     }
 
-    void traits(Class... classes) {
-        traits.addAll(classes)
+    /**
+     * Adds to the set of traits to be applied to the {@link Vertex} {@link #getTraits()} is used to get the {@link Set}.
+     * Traits will be applied to the {@link Vertex} before the {@link Vertex} is configured. {@link #config} can be
+     * used to configure these traits.
+     * @param classes traits to apply
+     */
+    void traits(Class... traits) {
+        this.traits.addAll(traits)
     }
 
+    /**
+     * Adds to the names the {@link Vertex} should connect to.
+     * @param names
+     */
     void connectsTo(String... names) {
         connectsTo.addAll(names)
     }
 
+    /**
+     * Either appends to or sets the config member. This config is applied to the {@link Vertex} after traits are applied.
+     * @param config added to the config member variable
+     */
     void config(@DelegatesTo(Vertex) Closure config) {
         if(this.config) {
             this.config << config
@@ -38,6 +68,12 @@ class VertexSpec {
         }
     }
 
+    /**
+     * Apply this specification to the given {@link Graph} and {@link Vertex}. First traits are applied. Then edges are
+     * created from connectsTo. Finally the config is applied to the {@link Vertex} using the leftShift operator.
+     * @param graph
+     * @param vertex
+     */
     void applyToGraphAndVertex(Graph graph, Vertex vertex) {
         if (traits) {
             vertex.delegateAs(traits as Class[])
@@ -50,6 +86,13 @@ class VertexSpec {
         }
     }
 
+    /**
+     * Creates a new instance of a VertexSpec using the provided closure. This object will be the delegate while the
+     * {@link Graph} will be the owner and this.
+     * @param graph
+     * @param closure
+     * @return the resulting VertexSpec
+     */
     static VertexSpec newInstance(Graph graph, @DelegatesTo(VertexSpec) Closure closure) {
         VertexSpec spec = new VertexSpec()
 
@@ -59,6 +102,17 @@ class VertexSpec {
         spec
     }
 
+    /**
+     * creates a new instance of a VertexSpec using the provided Map. Valid values that can be in the Map are:
+     * <p>
+     * traits - list of traits to be applied to the {@link Vertex}<br>
+     * connectsTo - list of vertices to connect the {@link Vertex} to.<br>
+     * config - closure to be applied to the {@link Vertex} after traits and edges are created.
+     * <p>
+     * All other values are ignored.
+     * @param map
+     * @return
+     */
     static VertexSpec newInstance(Map<String, ?> map) {
         VertexSpec spec = new VertexSpec(name:map.name)
         if (map.traits) {
