85545423a7815b3277a07823509ae6b0b124d13d
John Mercier <moaxcp@gmail.com>
2017-09-12 23:34:45 -0400

    Adding DslScript and tests using it in a script.

---
+++ src/test/groovy/script/ScriptWithBaseScript.groovy
@@ -0,0 +1,28 @@
+package script
+
+import graph.Graph
+import spock.lang.Specification
+
+class ScriptWithBaseScript extends Specification {
+    GroovyShell shell = new GroovyShell(this.class.classLoader)
+
+    def 'can change BaseScript'() {
+        expect:
+        shell.evaluate '''
+            import graph.DslScript
+            @groovy.transform.BaseScript DslScript graph
+        '''
+    }
+
+    def 'can change graph with BaseScript'() {
+        expect:
+        Graph graph = shell.evaluate '''
+            import graph.DslScript
+            @groovy.transform.BaseScript DslScript script
+            edge a, b
+            assert vertices.size() == 2
+            assert vertices['a'].name == 'a'
+            assert vertices['b'].name == 'b'
+        '''
+    }
+}
